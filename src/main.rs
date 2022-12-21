c5cd0f03ceff766f0639c174c958faba97c1bc9d
Andy Gayton <andy@thecablelounge.com>
2022-12-21 00:27:14 -0500

    parse some json and match on it

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 pub fn main() -> Result<(), Box<rhai::EvalAltResult>> {
     let engine = rhai::Engine::new();
-    let script = "print(40 + 2);";
+    let script = r#"
+        let s = `{"path": "/foo"}`;
+        let meta = parse_json(s);
+        let route = switch meta.path {
+            "/" => "index.html",
+            "/foo" => "foo.html",
+            _ => "404.sh"
+        };
+        print(route);
+    "#;
     engine.run(script)?;
     Ok(())
 }
