b3ba2c93d0d5ee8f3afbb18003918b42a45ccb0e
Andy Gayton <andy@thecablelounge.com>
2022-12-23 20:20:52 -0500

    make reading from stdin line by line available in rhai

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::{Arc, Mutex};
+
 use clap::{AppSettings, Parser};
 
 use rhai::plugin::*;
@@ -10,93 +12,41 @@ struct Args {
     path: std::path::PathBuf,
 }
 
-// My custom type
 #[derive(Clone)]
-pub struct TestStruct {
-    pub value: i64,
-}
-
 pub struct Reader {
-    reader:  Box<dyn std::io::BufRead>,
-    buffer: String,
+    reader: Arc<Mutex<Box<dyn std::io::BufRead>>>,
 }
 
 impl Reader {
     fn new(reader: Box<dyn std::io::BufRead>) -> Reader {
         Reader {
-            reader: reader,
-            buffer: String::new(),
+            reader: Arc::new(Mutex::new(reader)),
         }
     }
 
-    fn line<'f>(&'f mut self) -> &'f str {
-        self.buffer.clear();
-        self.reader.read_line(&mut self.buffer).unwrap();
-        self.buffer.as_str()
+    pub fn line(&mut self) -> rhai::ImmutableString {
+        let mut buffer = String::new();
+        self.reader.lock().unwrap().read_line(&mut buffer).unwrap();
+        buffer.into()
     }
 }
 
 #[export_module]
 mod my_module {
-    // This type alias will register the friendly name 'ABC' for the
-    // custom type 'TestStruct'.
-    pub type ABC = TestStruct;
-
-    // This constant will be registered as the constant variable 'MY_NUMBER'.
-    // Ignored when registered as a global module.
-    pub const MY_NUMBER: i64 = 42;
-
-    // This function will be registered as 'greet'
-    // but is only available with the 'greetings' feature.
-    pub fn greet(name: &str) -> String {
-        format!("hello, {}!", name)
-    }
-    /// This function will be registered as 'get_num'.
-    ///
-    /// If this is a Rust doc-comment, then it is included in the metadata.
-    pub fn get_num() -> i64 {
-        mystic_number()
+    pub fn stdin() -> Reader {
+        let stdin = std::io::stdin().lock();
+        Reader::new(Box::new(stdin))
     }
 
-    // pub fn stdin() ->
-
-
-    /// This function will be registered as 'create_abc'.
-    pub fn create_abc(value: i64) -> ABC {
-        ABC { value }
-    }
-    /// This function will be registered as the 'value' property of type 'ABC'.
-    #[rhai_fn(get = "value")]
-    pub fn get_value(ts: &mut ABC) -> i64 {
-        ts.value
-    }
-    // This function will be registered as 'increment'.
-    // It will also be exposed to the global namespace since 'global' is set.
     #[rhai_fn(global)]
-    pub fn increment(ts: &mut ABC) {
-        ts.value += 1;
-    }
-    // This function is not 'pub', so NOT registered.
-    fn mystic_number() -> i64 {
-        42
-    }
-
-    // Sub-modules are ignored when the module is registered globally.
-    pub mod my_sub_module {
-        // This function is ignored when registered globally.
-        // Otherwise it is a valid registered function under a sub-module.
-        pub fn get_info() -> String {
-            "say what".to_string()
-        }
+    pub fn line(ts: &mut Reader) -> rhai::ImmutableString {
+        let mut buffer = String::new();
+        ts.reader.lock().unwrap().read_line(&mut buffer).unwrap();
+        buffer.into()
     }
 }
 
 pub fn main() -> Result<(), Box<rhai::EvalAltResult>> {
-    let stdin = std::io::stdin().lock();
-    let mut stdin = Reader::new(Box::new(stdin));
-    print!("{}", stdin.line());
-    print!("{}", stdin.line());
-
     let args = Args::parse();
 
     let mut engine = rhai::Engine::new();
